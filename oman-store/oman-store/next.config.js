8d3505531c2d0a46f731420adf77f87c06151b27
nizarkhalid2 <nizarkhalid321@gmail.com>
2026-09-03 13:34:14 +0400

    Update next.config.js

--- oman-store/oman-store/next.config.js
+++ oman-store/oman-store/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com" },
       { protocol: "https", hostname: "**.aliexpress.com" },
@@ -8,4 +9,5 @@ const nextConfig = {
     ],
   },
 };
+
 module.exports = nextConfig;
