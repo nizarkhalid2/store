19d3dc9ee222b08c40d7cc5b577155c52d1bef59
nizarkhalid2 <nizarkhalid321@gmail.com>
2026-09-05 15:53:55 +0400

    Delete oman-store directory

--- oman-store/src/lib/db.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-// Prevent multiple Prisma instances in dev (Next.js hot reload)
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
-
-export const db = globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
